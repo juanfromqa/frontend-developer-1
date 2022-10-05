e0cef494a20c872f5357de31c08b061183d25db3
j <j@mb.local>
2022-10-04 21:36:12 -0500

    manipulacion del DOM basico

--- EjerciciosJavascript/script.js
+++ EjerciciosJavascript/script.js
@@ -1,1 +1,26 @@
-console.log('Hola');
+const h1 = document.querySelector('h1');
+const input1 = document.getElementById('num1');
+const input2 = document.getElementById('num2');
+const resultado = document.getElementById('resultado');
+const button = document.getElementById('btnCalcular')
+const form = document.getElementById('form')
+
+h1.innerHTML = 'Patito'
+h1.setAttribute('data-testid', "header")
+h1.classList.add('verde')
+
+
+const img = document.createElement('img');
+
+img.setAttribute('src', '/Users/j/src/frontend-developer-1/EjerciciosFrontEnd/logos/favicon_yard_sale.svg');
+console.log(img)
+
+// pid.append(img)
+
+form.addEventListener('submit', btnOnClick)
+
+function btnOnClick(event){
+    event.preventDefault()
+    const res = input1.value + input2.value
+    resultado.innerText = "Resultado = " + res;
+}
